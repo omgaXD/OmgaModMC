87b368463e51f396e549b3f76934929d4cdf39e4
omga <sladekop@gmail.com>
2022-11-25 21:15:02 +0500

    - Removed info bar about goldsteel ingot crafting. - Added more tooltips and edited previous ones.

--- run/kubejs/client_scripts/script.js
+++ run/kubejs/client_scripts/script.js
@@ -8,9 +8,3 @@ onEvent('jei.remove.recipes', event => {
 		event.remove('create:spout_filling','create:filling/fertilizer_spray_' + i)
 	}
 })
-
-onEvent('jei.information', event => {
-	event.add('omgamod:goldsteel_blend', ['Dip goldsteel blend into honey, then blazing blood, then molten gold to turn it into goldsteel ingot.'])
-	event.add('omgamod:goldsteel_ingot', ['Dip goldsteel blend into honey, then blazing blood, then molten gold to turn it into goldsteel ingot.'])
-	
-})
--- run/kubejs/client_scripts/tooltips.js
+++ run/kubejs/client_scripts/tooltips.js
@@ -3,11 +3,15 @@ let tooltip = null;
 const addTooltip = (item, str) => {
     tooltip.addAdvanced(item, (item, advanced, text) => {
     if (!tooltip.shift) {
-        text.add(1, Text.gray('[Shift for more info]').italic())
+        text.add(1, ('§8Hold [§7Shift§8] for summary'))
     } else {
-        let i = 1;
+        text.add(1, ('§8Hold [§fShift§8] for summary'))
+        text.add(2, '')
+        let i = 3
         str.forEach(st => {
-            text.add(st);
+
+            text.add(i, '§4'+st);
+            i++;
         });
     }
     })
@@ -19,5 +23,13 @@ onEvent("item.tooltip", t => {
     addTooltip('omgamod:redsteel_block', ["Can act as a strong light source when lit by redstone."])
     addTooltip('omgamod:woodsteel_block', ["Can be used as a plant base.", "Saplings planted on it will grow faster."])
     addTooltip('omgamod:goldsteel_block', ["Can be used in alchemy as philosopher's stone:", "Throw it into any metal fluid source, get molten gold!"])
+
+    addTooltip('omgamod:fertilizer_spray', ["Can be used as bonemeal. Should be recharged by spouting fertilizer fluid on it."])
+    addTooltip('omgamod:fertilizer_spray_empty', ["An empty bottle with spray. Supposed to be filled with fertilizer fluid"])
+    addTooltip('omgamod:tnt_cannon', ["Can shoot TNT or TNT minecarts!", "(Latter is NOT recommended)."])
+
+    addTooltip('omgamod:saturation_fruit', ["Gives 'Saturation' effect which restores most of your hunger"])
+
+    
     //*/
 })
